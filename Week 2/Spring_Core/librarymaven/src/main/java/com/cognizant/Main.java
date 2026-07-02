ec77e6243e72399d25d9778f09adcbe9fbe6d4ef
Priyanshu <hkm780414@gmail.com>
2026-07-02 22:44:23 +0530

    week 2: Spring core - library module with maven

---
+++ Week 2/Spring_Core/librarymaven/src/main/java/com/cognizant/Main.java
@@ -0,0 +1,8 @@
+package com.cognizant;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This is a Maven project for Spring Core Library Management.");
+    }
+}
