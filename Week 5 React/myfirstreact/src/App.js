6c2167f81c115d96d53e73858ba9215b9608e3d6
Priyanshu <hkm780414@gmail.com>
2026-07-16 17:52:17 +0530

    Week 5 - React Handson First project

---
+++ Week 5 React/myfirstreact/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+
+function App() {
+  return (
+    <div>
+      {/*-------------------- Hands on 1----------------------- */}
+      {/* <h1>Welcome to the first session of React</h1> */}
+      
+      
+      {/*-------------------- Hands on 2----------------------- */}
+      <Home />
+      <About />
+      <Contact />
+      
+    </div>
+
+  );
+}
+
+export default App;
