e78bcdf67b5e1cd5f3d03469979225eb6d316fb1
Priyanshu <hkm780414@gmail.com>
2026-07-23 10:28:12 +0530

    React blogapp handson 4

---
+++ Week 5 React/blogapp/src/Post.js
@@ -0,0 +1,12 @@
+/*------------ Hands-on 4 ------------------------*/
+
+class Post {
+  constructor(userId, id, title, body) {
+    this.userId = userId;
+    this.id = id;
+    this.title = title;
+    this.body = body;
+  }
+}
+
+export default Post;
