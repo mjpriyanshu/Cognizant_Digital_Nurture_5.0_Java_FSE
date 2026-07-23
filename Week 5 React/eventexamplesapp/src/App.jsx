4d65eb10a0c33a43b275d8d5ae708e746ecc67ed
Priyanshu <hkm780414@gmail.com>
2026-07-23 11:01:59 +0530

    Events in react

---
+++ Week 5 React/eventexamplesapp/src/App.jsx
@@ -0,0 +1,18 @@
+/*------------ Hands-on 11 ------------------------*/
+
+import EventExamples from "./Components/EventExamples";
+import CurrencyConverter from "./Components/CurrencyConvertor";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <EventExamples />
+
+      <hr />
+
+      <CurrencyConverter />
+    </div>
+  );
+}
+
+export default App;
---
+++ Week 5 React/eventexamplesapp/src/Components/EventExamples.jsx
@@ -0,0 +1,76 @@
+/*------------ Hands-on 11 ------------------------*/
+
+import React, { Component } from "react";
+
+class EventExamples extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
+  sayHello = () => {
+    alert("Hello! Have a nice day.");
+  };
+
+  incrementAndHello = () => {
+    this.increment();
+    this.sayHello();
+  };
+
+  sayWelcome = (message) => {
+    alert(message);
+  };
+
+  onPress = () => {
+    alert("I was clicked");
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Counter : {this.state.count}</h2>
+
+        <button onClick={this.incrementAndHello}>
+          Increment
+        </button>
+
+        {"  "}
+
+        <button onClick={this.decrement}>
+          Decrement
+        </button>
+
+        <br /><br />
+
+        <button
+          onClick={() => this.sayWelcome("Welcome")}
+        >
+          Say Welcome
+        </button>
+
+        <br /><br />
+
+        <button onClick={this.onPress}>
+          OnPress
+        </button>
+      </div>
+    );
+  }
+}
+
+export default EventExamples;
